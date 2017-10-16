8c71473e42ec06ca88a2dccd1dda26c9a3c800dd
zabeen <zabeen.patel@ucl.ac.uk>
2017-10-16 01:21:51 +0100

    Added a simple multiplier

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,7 +7,34 @@ namespace Calculator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the calculator!");
+
+            // prompt user to provide two numbers
+            Console.WriteLine("1st Number:");
+            string firstNo = Console.ReadLine();
+
+            Console.WriteLine("2nd Number:");
+            string secondNo = Console.ReadLine();
+
+            // Convert numbers to ints - catch error if non-int submitted
+            int x;
+            int y;
+
+            try
+            {
+                x = int.Parse(firstNo);
+                y = int.Parse(secondNo);
+
+                // multiple the two numbers and write the product
+                Console.WriteLine("Result: {0}", x * y);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Only integers allowed.");
+            }
+
+            // Wait
             Console.ReadLine();
+
         }
     }
 }
