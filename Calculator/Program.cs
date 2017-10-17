18ad58160b871a44618dfebcdaf12f006d00c4ea
zabeen <zabeen.patel@ucl.ac.uk>
2017-10-17 22:31:22 +0100

    Calculator code refactored, and user can now keep running calculator until they quit.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -5,98 +5,125 @@ namespace Calculator
 {
     class Program
     {
+        static List<string> operators = new List<string> { "+", "-", "*", "/" };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the calculator!\n==========================");
+            PrintWelcomeMessage();
 
-            // Request inputs - handle undesired values
-            Console.Write("Please enter the operator: ");
-            String op = Console.ReadLine();
-
-            if (!new List<string> { "+", "-", "*", "/" }.Contains(op))
+            bool continueCalc = false;
+            do
             {
-                Console.WriteLine("Error: unexpected operator. Only +, -, * or / accepted.");
-                return;
+                PerformOneCalculation();
+                continueCalc = ShouldContinue();
             }
+            while (continueCalc);
 
-            Console.Write("How many numbers do you want to {0}? ", op);
-            String noRequested = Console.ReadLine();
-            int n = 0;
-
-            if (!CheckStrIsInt(noRequested, out n))
-                return;
-            
-            // ask for operand and apply to answer
-            bool errorFlag = false;
-            int answer = 0;
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("Please enter number {0}: ", i);
-                String operandStr = Console.ReadLine();
-                int operand = 0;
-
-                // only perform calculation if submitted number is an int
-                if (CheckStrIsInt(operandStr, out operand))
-                {
-                    // Set answer var to val of first input on first loop
-                    if (i == 0)
-                    {
-                        answer = operand;
-                    }
-                    else
-                    {
-                        // set calculation by operator
-                        switch (op)
-                        {
-                            case "+":
-                                answer += operand;
-                                break;
-                            case "-":
-                                answer -= operand;
-                                break;
-                            case "*":
-                                answer *= operand;
-                                break;
-                            case "/":
-                                answer /= operand;
-                                break;
-                            default:
-                                break;
-                        }
-                    }
-                }
-                else
-                {
-                    errorFlag = true;
-                    break;
-                }
-            }
-
-            // if no errors encountered, write out final answer
-            if (!errorFlag)
-                Console.WriteLine("The answer is {0}: ", answer);
+            PrintFarewellMessage();
 
             // Wait
             Console.ReadLine();
-
         }
 
-        private static bool CheckStrIsInt(string nStr, out int n)
+        private static void PrintWelcomeMessage()
         {
-            bool isInt = false;
-            n = 0;
+            Console.WriteLine("Welcome to the calculator!\n==========================");
+        }
 
-            try
+        private static void PerformOneCalculation()
+        {
+            // Request inputs
+            string operatorStr = GetOperatorFromUser();
+            int noOfOperands = GetIntegerFromUser("How many numbers do you want to " + operatorStr + "? ");
+            decimal answer = CalculateAnswer(operatorStr, noOfOperands);
+
+            // write out final answer
+            Console.WriteLine("The answer is {0}.", answer);
+        }
+
+        private static string GetOperatorFromUser()
+        {
+            String op = "";
+            do
             {
-                n = int.Parse(nStr);
-                isInt = true;
+                Console.Write("Please enter the operator: ");
+                op = Console.ReadLine();
             }
-            catch (FormatException)
+            while (!operators.Contains(op));
+
+            return op;
+        }
+
+        private static int GetIntegerFromUser(string messageStr)
+        {
+            int submittedInt = 0;
+            String input = "";
+            do
             {
-                Console.WriteLine("Error: {0} is not an integer.", nStr);
+                Console.Write(messageStr);
+                input = Console.ReadLine();
+            }
+            while (!int.TryParse(input, out submittedInt));
+
+            return submittedInt;
+        }
+
+        private static decimal CalculateAnswer (string operatorStr, int noOfOperands)
+        {
+            decimal answer = 0;
+            for (int i = 0; i < noOfOperands; i++)
+            {
+                // ask for operand
+                int operand = GetIntegerFromUser("Please enter number " + (i + 1) + ": ");
+
+                // Set answer var to val of first input on first loop
+                if (i == 0)
+                {
+                    answer = operand;
+                }
+                else
+                {
+                    // set calculation by operator
+                    switch (operatorStr)
+                    {
+                        case "+":
+                            answer += operand;
+                            break;
+                        case "-":
+                            answer -= operand;
+                            break;
+                        case "*":
+                            answer *= operand;
+                            break;
+                        case "/":
+                            answer /= operand;
+                            break;
+                        default:
+                            break;
+                    }
+                }
             }
 
-            return isInt;
+            return answer;
+        }
+
+        private static bool ShouldContinue()
+        {
+            string response = "";
+            // keep asking user for response until they type either 'y' or 'n'
+            do
+            {
+                Console.WriteLine("Do you wish to continue? y/n");
+                response = Console.ReadLine().ToLower();
+            }
+            while (response != "y" && response != "n");
+
+            return (response == "y");
+        }
+
+        private static void PrintFarewellMessage()
+        {
+            Console.WriteLine("Goodbye!");
         }
     }
 }
